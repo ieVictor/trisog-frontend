b1eb0d48fa0f31da52364b4f292b3438fdc628b9
Victor Gabriel <victor_tiss@outlook.com>
2024-08-20 20:17:29 -0300

    feat(FifthSection): add fifth section initial implementation

---
+++ src/pages/Home/FifthSection/index.tsx
@@ -0,0 +1,43 @@
+import { Quotes } from '@phosphor-icons/react'
+import PhotoDump from '../../../assets/PhotoDump.png'
+import CenteredTitle from '../../../components/CenteredTitle'
+import Slider from 'react-slick'
+import { Quotation } from './athoms/Quotation'
+
+export default function FifthSection() {
+  return (
+    <section className="w-full flex flex-row justify-center gap-36 py-20 bg-gray-100">
+      <img src={PhotoDump} alt="Photos"/>
+      <div className='w-full flex flex-col max-w-[482px] gap-4'>
+        <div className='w-full flex flex-col items-center gap-12'>
+          <CenteredTitle title='What Travelers Say' subtitle='Testimonials'/>
+          <Quotes size={32} className='text-rose-500'/>
+        </div>
+        <Slider
+          infinite={false} 
+          dotsClass="slick-dots -bottom-16 text-rose-500"
+          dots
+          arrows={false}
+          speed={500}
+          slidesToShow={1}
+        >
+          <Quotation author='-By Molie Rosa, Photographer'>
+           “The UI designs he crafted are top-notch, 
+           and the design system he integrated allows for straight forward fixes and bulk 
+           updates throughout almost every area of the app.”   
+          </Quotation>
+          <Quotation author='-By Molie Rosa, Photographer'>
+           “The UI designs he crafted are top-notch, 
+           and the design system he integrated allows for straight forward fixes and bulk 
+           updates throughout almost every area of the app.”   
+          </Quotation>
+          <Quotation author='-By Molie Rosa, Photographer'>
+           “The UI designs he crafted are top-notch, 
+           and the design system he integrated allows for straight forward fixes and bulk 
+           updates throughout almost every area of the app.”   
+          </Quotation>
+        </Slider>
+      </div>
+    </section>
+  )
+}
