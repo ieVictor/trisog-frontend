36011445a9bd3ac78d75980d176a4c27d6eeb8ff
Victor Gabriel <victor_tiss@outlook.com>
2024-08-20 15:42:33 -0300

    style(GaleryItem): add background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,7 @@ export default {
     extend: {
       backgroundImage: {
         'home-first-section': "linear-gradient(to bottom, rgba(40, 38, 62, 50%), rgba(8, 4, 51, 80%)), url('./src/assets/firstSection-bg.jpg')",
+        'galeryItem': "linear-gradient(to bottom, rgba(0, 0, 0, 0%), rgba(0, 0, 0, 100%)), url('./src/assets/tourCardPlaceholder.png')",
       },
     },
   },
