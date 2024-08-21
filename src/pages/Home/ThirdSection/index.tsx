730f19a824da87bd4893134277fadfaf058408f0
Victor Gabriel <victor_tiss@outlook.com>
2024-08-21 10:38:28 -0300

    refactor(ThirdSection): use CenteredTitle component

--- src/pages/Home/ThirdSection/index.tsx
+++ src/pages/Home/ThirdSection/index.tsx
@@ -1,13 +1,10 @@
-import CenteredSubtitle from "../../../components/CenteredSubtitle";
+import CenteredTitle from "../../../components/CenteredTitle";
 import GaleryItem from "./components/GaleryItem";
 
 export default function ThirdSection() {
   return (
     <section className="w-full bg-gray-100 h-[772px] gap-16 px-32 py-16 flex flex-col items-center justify-center">
-      <div className="flex flex-col gap-2 justify-center">
-        <CenteredSubtitle content="Destination"/>
-        <h1 className="text-4xl text-blue-950 font-display text-center font-bold">Top Atractions Destination</h1>
-      </div>
+      <CenteredTitle title="Top Atractions Destination" subtitle="Destination"/> 
       <div className="w-full h-full flex flex-row gap-8">
         <div className="flex flex-col gap-8 w-full h-full">
           <div className="flex flex-row gap-8 h-full">
