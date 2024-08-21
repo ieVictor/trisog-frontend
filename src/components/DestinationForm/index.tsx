43218ca3c8144da56150d3817ce16a9ad2f8bd0d
Victor Gabriel <victor_tiss@outlook.com>
2024-08-21 10:31:20 -0300

    style(DestinationForm): add drop shadow

--- src/components/DestinationForm/index.tsx
+++ src/components/DestinationForm/index.tsx
@@ -3,7 +3,12 @@ import TextInput from "../TextInput";
 
 export default function DestinationForm() {
   return (
-    <form className="absolute flex gap-5 px-6 h-[110px] items-center self-center bg-white -bottom-10 rounded-xl border-gray-500">
+    <form 
+      className={`
+        absolute flex gap-5 px-6 h-[110px] items-center self-center bg-white -bottom-10
+        rounded-xl border-gray-500 shadow-md
+      `}
+    >
       <div className="gap-5 flex items-end">
         <TextInput label="Destination" icon="plane" placeholder="Where to go?"/>
         <TextInput label="Type" icon="flag" placeholder="Activity"/>
