d0ed7f1ed13eac31871310bd8cd46caca6dfc06e
Victor Gabriel <victor_tiss@outlook.com>
2024-08-21 10:35:49 -0300

    feat(Rating): add filled star icon

--- src/components/TourCard/athoms/Rating.tsx
+++ src/components/TourCard/athoms/Rating.tsx
@@ -1,11 +1,14 @@
-import { Star } from "@phosphor-icons/react";
+import StarFilled from '../../../assets/StarFilled.svg'
 
-export default function Rating() {
+type RatingsProps = {
+  rating: number;
+}
+
+export default function Rating(props: RatingsProps) {
   return (
     <div className="flex flex-row items-center px-1 py-1 bg-rose-400 text-white rounded">
-      <Star size={12}/>
-      <span className="font-sans text-xs">4.6</span>
+      <img src={StarFilled} alt="Rating Star" width={12}/>
+      <span className="font-sans text-xs">{ props.rating }</span>
     </div>
   )
-
 }
