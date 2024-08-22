adfb845b188ae30642086e83cf6e293d2ae3b778
Victor Gabriel <victor_tiss@outlook.com>
2024-08-22 11:52:59 -0300

    feat(TourPackage/form): add search & slider form initial implementation

---
+++ src/pages/TourPackage/components/SearchForm.tsx
@@ -0,0 +1,11 @@
+import InputContainer from "../athoms/InputContainer";
+import SearchInput from "../athoms/SearchInput";
+
+export default function SearchForm() {
+  return (
+    <InputContainer>
+      <h6>Search</h6>
+      <SearchInput placeholder="Type anything..."/>
+    </InputContainer>
+  )
+} 
