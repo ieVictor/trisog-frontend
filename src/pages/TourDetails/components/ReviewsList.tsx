2fcd1888b09fbc5a081cedc00f6eaeadf0e5d56f
Victor Gabriel <victor_tiss@outlook.com>
2024-08-24 15:11:51 -0300

    feat(TourDetails): add ReviewsList container component

---
+++ src/pages/TourDetails/components/ReviewsList.tsx
@@ -0,0 +1,14 @@
+type ReviewsListProps = {
+  children: JSX.Element
+}
+
+export default function ReviewsList(props: ReviewsListProps) {
+  return (
+    <div className="flex flex-col gap-3">
+      <h6 className="text-h6 text-blue-950">Showing 1 review</h6>
+      <div className="w-full flex flex-col gap-2">
+        {props.children}
+      </div>
+    </div>
+  )
+}
