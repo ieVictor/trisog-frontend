26275ae0f4993fc637527aed22d50fe46979c599
Victor Gabriel <victor_tiss@outlook.com>
2024-08-24 14:42:09 -0300

    style(Button): add width fit/full property

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,18 +3,20 @@ type ButtonProps = {
   onClick: () => void;
   disabled?: boolean;
   type: 'submit' | 'button' | 'reset';
+  fill?: boolean
 };
 
-const Button = ({ text, type, onClick, disabled }: ButtonProps) => {
+const Button = ({ text, type, onClick, disabled, fill}: ButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
       disabled={disabled}
-      className={`
-        flex w-fit px-7 items-center text-white bg-rose-500 
-        font-sans text-center rounded-lg h-11 transition hover:brightness-90
-      `}
+      className={
+        "flex  px-7 items-center text-white bg-rose-500 " +
+        "font-sans text-center rounded-lg h-11 transition hover:brightness-90 " +
+        (fill ? "w-full" : "w-fit")
+      }
     >
       <span className="flex-1 font-sans font-normal text-base">{text}</span>
     </button>
