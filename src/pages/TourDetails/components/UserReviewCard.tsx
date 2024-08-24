19a121cf79260ef5f5cb4fb7820b188d27c1bf6e
Victor Gabriel <victor_tiss@outlook.com>
2024-08-24 15:11:15 -0300

    feat(TourDetails): add userReviewCard component

---
+++ src/pages/TourDetails/components/UserReviewCard.tsx
@@ -0,0 +1,39 @@
+import Rating from "../../../components/TourCard/athoms/Rating";
+
+type UserReviewCardProps = {
+  user: {
+    firstName: string;
+    lastName?: string;
+    img?: string;
+    reviewsCounter?: number;
+  } 
+  date: string
+  rating: number
+  children: string 
+}
+export default function UserReviewCard(props: UserReviewCardProps) {
+  return (
+    <div className="flex flex-row gap-4 p-8 w-full border-gray-200 border">
+      <img src={props.user.img} alt="userImage" className="rounded-full w-24 h-24"/>
+      <div className="flex flex-col gap-4">
+        <hgroup className="flex flex-col gap-2">
+          <span 
+            className="text-caption text-gray-400"
+          >
+            {props.date}
+          </span>
+          <h6 className="text-h6 text-blue-950">
+            {props.user.firstName + " " + props.user.lastName}
+          </h6>
+          <span className="flex flex-row gap-2 text-body-s text-gray-400">
+            <Rating rating={props.rating}/>
+            {props.user.reviewsCounter + " reviews"} 
+          </span>
+        </hgroup>
+        <p className="text-body text-gray-500 w-full">
+          {props.children}
+        </p>
+      </div>
+    </div>
+  )
+}
