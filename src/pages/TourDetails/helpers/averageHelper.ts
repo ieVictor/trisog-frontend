e38844bd73e87a3c44a179719398641adc2fcde5
Victor Gabriel <victor_tiss@outlook.com>
2024-08-24 15:15:33 -0300

    feat(TourDetails): add helpers for tour details page

---
+++ src/pages/TourDetails/helpers/averageHelper.ts
@@ -0,0 +1,39 @@
+export type AverageColors =
+  | 'bg-rose-500'
+  | 'bg-rose-400'
+  | 'bg-gray-400'
+
+export type AverageTexts =
+  | "Bad"
+  | "Ok"
+  | "Nice"
+  | "Good"
+  | "Excellent"
+  | "Perfect"
+
+const AVERAGE_DISPLAY_COLORS: Record<number, AverageColors> = {
+  5: "bg-rose-500",
+  4: "bg-rose-500",
+  3: "bg-gray-400",
+  2: "bg-gray-400",
+  1: "bg-gray-400",
+  0: "bg-gray-400"
+}
+
+const AVERAGE_DISPLAY_TEXTS: Record<number, AverageTexts> = {
+  5: "Perfect",
+  4: "Excellent",
+  3: "Good",
+  2: "Nice",
+  1: "Ok",
+  0: "Bad"
+}
+
+
+export function getTextByAverage(Average: number): AverageTexts {
+  return AVERAGE_DISPLAY_TEXTS[Average]
+}
+
+export function getColorByAverage(Average: number): AverageColors {
+  return AVERAGE_DISPLAY_COLORS[Average];
+}
