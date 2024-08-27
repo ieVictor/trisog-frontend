631bf56e4aee68de26e3663636f9aa1f6dad5d48
Victor Gabriel <victor_tiss@outlook.com>
2024-08-27 18:22:06 -0300

    feat(routes): add initial router implementation

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './styles/index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import TourPackage from './pages/TourPackage/index.tsx'
+import TourDetails from './pages/TourDetails/index.tsx'
+import { TourModel } from './models/tourModel.ts'
+import Home from './pages/Home/index.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/tours",
+        element: <TourPackage/>,
+      },
+      {
+        path: "/tours/:id",
+        element: <TourDetails data={TourModel.data}/>,
+      },
+    ],
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
