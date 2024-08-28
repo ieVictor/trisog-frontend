a11b8bcb67c71e783f78895e80fae56141069799
Victor Gabriel <victor_tiss@outlook.com>
2024-08-28 14:32:35 -0300

    feat(user): add user profile icon

---
+++ src/components/Navigation/athoms/UserProfile.tsx
@@ -0,0 +1,51 @@
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Tooltip } from "@nextui-org/react"
+import { useUser } from "../../../hooks/userHooks"
+import { SignOut } from "@phosphor-icons/react"
+
+type UserProfileProps = {
+ imgURL?: string
+ username?: string
+}
+
+export default function UserProfile(props: UserProfileProps) {
+  const DEFAULT_URL = 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png'
+  const userHooks = useUser();
+
+  const handleLogout = () => {
+    userHooks.auth.signOut();
+  }
+
+  return (
+    <Tooltip
+      content={props.username}
+      color='danger'
+    >
+      <Dropdown>
+        <DropdownTrigger>
+          <Avatar 
+            src={props.imgURL === DEFAULT_URL ? '' : props.imgURL} 
+            name={props.imgURL === DEFAULT_URL ? props.username : ''}
+            color="danger"
+            className='rounded-full cursor-pointer'
+            size="sm"
+            isBordered
+            radius="sm"
+          />
+        </DropdownTrigger>
+        <DropdownMenu variant="bordered" color="primary">
+          <DropdownItem 
+            key="logout" 
+            className="text-gray-800" 
+            color="danger" 
+            onClick={handleLogout} 
+            endContent={(
+            <SignOut size={16}/>
+            )}
+          >
+            Log out
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+    </Tooltip>
+  )
+} 
