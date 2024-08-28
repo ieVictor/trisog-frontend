f62f428f7cfbf777c1b80c43b6263184360dda02
Victor Gabriel <victor_tiss@outlook.com>
2024-08-28 14:54:20 -0300

    refactor(privateRoute): change privateRoute implementation

--- src/utils/PrivateRoute.tsx
+++ src/utils/PrivateRoute.tsx
@@ -1,17 +1,7 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useUser } from '../hooks/userHooks';
-import Navigation from '../components/Navigation';
-import Modal from '../components/Modal';
 
 export function PrivateRoute() {
   const { user } = useUser();
-  return user ? (
-    <>
-      <Navigation />
-      <Modal />
-      <Outlet />
-    </>
-  ) : (
-    <Navigate to="/" />
-  );
+  return user ? <Outlet /> : <Navigate to="/" />
 }
