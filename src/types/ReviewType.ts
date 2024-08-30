82d0fa138a528538db864b79d90097cf2aa801ab
Victor Gabriel <victor_tiss@outlook.com>
2024-08-30 08:35:58 -0300

    feat(review): add new fields to review type

--- src/types/ReviewType.ts
+++ src/types/ReviewType.ts
@@ -3,6 +3,8 @@ export type Review = {
   tourId: string;
   userId: string;
   overview: string;
+  creatorName: string;
+  creatorEmail: string;
 
   average: number;
   services: number;
@@ -18,6 +20,21 @@ export type Review = {
     email: string;
     username: string;
     img: string;
+    _count: { reviews: number };
   }
+
+  anonymous: boolean;
+} 
+
+export type ReviewInsert = {
+  tourId: string
+  userId: string;
+  overview: string;
+  services: number;
+  locations: number;
+  amenities: number;
+  prices: number;
+  food: number;
+  room: number;
 }
 
