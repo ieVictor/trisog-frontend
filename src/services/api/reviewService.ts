8bce5c90039f3b4ed378cc6f337130bb2c18b3ac
Victor Gabriel <victor_tiss@outlook.com>
2024-08-30 08:36:22 -0300

    feat(review): add createReview service

--- src/services/api/reviewService.ts
+++ src/services/api/reviewService.ts
@@ -1,14 +1,32 @@
 import axios, { AxiosResponse } from "axios";
 import { api } from "./api";
-import { Review } from "../../types/ReviewType";
+import { Review, ReviewInsert } from "../../types/ReviewType";
+
+interface ReviewsResponse extends Array<Review> {
+  msg?: string
+}
+
+interface ReviewResponse extends Review {
+  msg?: string
+}
+
 
 class ReviewService {
-  async getReviewsByTourId(tour_id: string): Promise<AxiosResponse<Review[]> | null> {
+  async getReviewsByTourId(tour_id: string): Promise<AxiosResponse<ReviewsResponse> | null> {
     try {
       const response = await api.get(`reviews/${tour_id}`);
       return response;
     } catch (error) {
       if (axios.isAxiosError(error) && error.response) return error.response;
+    }
+    return null
+  }
+  async createReview(data: ReviewInsert): Promise<AxiosResponse<ReviewResponse> | null> {
+    try {
+      const response = await api.post(`reviews`, {...data})
+      return response
+    } catch (error) {
+      if (axios.isAxiosError(error) && error.response) { return error.response };
     }
     return null
   }
