105edb5ece453795348afc290ae0b72323996549
Victor Gabriel <victor_tiss@outlook.com>
2024-09-02 11:26:26 -0300

    chore(checkListItem): format code

--- src/pages/TourPackage/athoms/CheckListItem.tsx
+++ src/pages/TourPackage/athoms/CheckListItem.tsx
@@ -2,13 +2,14 @@ import CheckBox from "./CheckBox";
 
 type ListItemProps = {
   label: string;
+  
 }
 
 export default function CheckListItem(props: ListItemProps) {
   return (
     <div className="flex items-center">
       <CheckBox/>
-      <label htmlFor="default-checkbox" className="ms-2 text-sm font-medium text-gray-600">{props.label}</label>
+      <label htmlFor="default-checkbox" className="ms-2 text-action text-gray-600">{props.label}</label>
     </div>
   )
 }
