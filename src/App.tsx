79fa00aa96aca6d38214822ab8d26ef42c65e236
Victor Gabriel <victor_tiss@outlook.com>
2024-09-02 11:29:39 -0300

    feat(App): use CategoriesProvider & add informations bar

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import './styles/App.css';
 import Modal from './components/Modal';
 import { NextUIProvider } from '@nextui-org/react';
 import { ModalProvider } from './contexts/ModalContext';
+import { CategoriesProvider } from './contexts/CategoriesContext';
+import InformationsBar from './components/InformationsBar';
 
 function App() {
   const { pathname } = useLocation();
@@ -15,9 +17,12 @@ function App() {
   return (
     <NextUIProvider locale="en-GB" className="flex flex-col relative">
         <ModalProvider>
+          <InformationsBar/>
           <Navigation />
           <Modal />
-          <Outlet />
+          <CategoriesProvider>
+            <Outlet />
+          </CategoriesProvider>
         </ModalProvider>
     </NextUIProvider>
   );
