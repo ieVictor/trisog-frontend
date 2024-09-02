7a543b05feb6cdb93047bb800360492066ed2b6a
Victor Gabriel <victor_tiss@outlook.com>
2024-09-02 14:05:58 -0300

    feat(links): add redirect links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,8 @@
-import { FacebookLogo, LinkedinLogo, TwitterLogo } from "@phosphor-icons/react"
-import WhiteLogo from "../../assets/trisog-white-logo.svg"
-import TextInput from "../TextInput"
-import Button from "../Button"
+import { FacebookLogo, LinkedinLogo, TwitterLogo } from '@phosphor-icons/react';
+import WhiteLogo from '../../assets/trisog-white-logo.svg';
+import TextInput from '../TextInput';
+import Button from '../Button';
+
 export default function Footer() {
   return (
     <>
@@ -9,9 +10,11 @@ export default function Footer() {
         <div className="w-full h-full flex flex-row gap-20">
           <div className="flex flex-col gap-5 max-w-[209px]">
             <div className="flex flex-col gap-8">
-              <img src={WhiteLogo} alt="Trisog White Logo"/>
+              <img src={WhiteLogo} alt="Trisog White Logo" />
               <div className="flex flex-col g-1 text-start">
-                <span className="font-script text-sm text-white">Need any help?</span>
+                <span className="font-script text-sm text-white">
+                  Need any help?
+                </span>
                 <span className="font-display text-lg text-white">
                   Call Us:
                   <strong className="text-rose-400">(888)1234 5678</strong>
@@ -19,61 +22,107 @@ export default function Footer() {
               </div>
             </div>
             <div className="text-white text-start items-start flex flex-col gap-7">
-              <span className="text-sans text-base">Love Street, Muscat, Oman exaample@trisog.com</span>
+              <span className="text-sans text-base">
+                Love Street, Muscat, Oman exaample@trisog.com
+              </span>
               <nav className="flex flex-row gap-3 justify-center">
-                <FacebookLogo size={20}/>
-                <TwitterLogo size={20}/>
-                <LinkedinLogo size={20}/>
+                <a
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FacebookLogo size={20} />
+                </a>
+                <a href="https://twitter.com/home" target="_blank">
+                  <TwitterLogo size={20} />
+                </a>
+                <a href="https://www.linkedin.com" target="_blank">
+                  <LinkedinLogo size={20} />
+                </a>
               </nav>
             </div>
           </div>
 
-          <div className="w-px h-60 bg-white"/>
+          <div className="w-px h-60 bg-white" />
 
           <div className="flex flex-row gap-16 items-center text-white">
             <div className="flex flex-col gap-4">
               <span className="font-script text-sm text-start">Company</span>
               <ul className="flex flex-col gap-3 font-sans text-start text-base text-white">
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>Travel Guides</li>
-                <li>Data Policy</li>
+                <li>
+                  <a className="hover-bright cursor-pointer">About Us</a>
+                </li>
+                <li>
+                  <a className="hover-bright cursor-pointer">Contact Us</a>
+                </li>
+                <li>
+                  <a className="hover-bright cursor-pointer">Travel Guides</a>
+                </li>
+                <li>
+                  <a className="hover-bright cursor-pointer">Data Policy</a>
+                </li>
               </ul>
             </div>
             <div className="flex flex-col gap-4">
-              <span className="font-script text-sm text-start">Top Destination</span>
+              <span className="font-script text-sm text-start">
+                Top Destination
+              </span>
               <div className="flex flex-row gap-8">
                 <ul className="flex flex-col gap-3 font-sans text-start text-base text-white">
-                  <li>Las Vegas</li>
-                  <li>New York City</li>
-                  <li>San Francisco</li>
-                  <li>Hawaii</li>
+                  <li>
+                    <a className="hover-bright cursor-pointer">Las Vegas</a>
+                  </li>
+                  <li>
+                    <a className="hover-bright cursor-pointer">New York City</a>
+                  </li>
+                  <li>
+                    <a className="hover-bright cursor-pointer">San Francisco</a>
+                  </li>
+                  <li>
+                    <a className="hover-bright cursor-pointer">Hawaii</a>
+                  </li>
                 </ul>
                 <ul className="flex flex-col gap-3 font-sans text-start text-base text-white">
-                  <li>Tokyo</li>
-                  <li>Sydney</li>
-                  <li>Melbourne</li>
-                  <li>Dubai</li>
+                  <li>
+                    <a className="hover-bright cursor-pointer">Tokyo</a>
+                  </li>
+                  <li>
+                    <a className="hover-bright cursor-pointer">Sydney</a>
+                  </li>
+                  <li>
+                    <a className="hover-bright cursor-pointer">Melbourne</a>
+                  </li>
+                  <li>
+                    <a className="hover-bright cursor-pointer">Dubai</a>
+                  </li>
                 </ul>
               </div>
             </div>
           </div>
 
-          <div className="w-px h-60 bg-white"/>
+          <div className="w-px h-60 bg-white" />
 
           <div className="flex flex-col gap-14 text-white">
             <form className="text-start flex flex-col gap-4">
               <span className="font-script text-sm">Sign up Newsletter</span>
               <div className="flex flex-col gap-2">
-                <TextInput icon="plane" placeholder="Enter Email..."/>
-                <Button text="Submit" type="button" onClick={() => {}}/>
+                <TextInput
+                  icon="plane"
+                  placeholder="Enter Email..."
+                  validate={
+                    /^((?!\.)[\w\-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gm
+                  }
+                />
+                <Button text="Submit" type="button" onClick={() => {}} />
               </div>
             </form>
-            <span className="text-gray-400 text-sm">© 2023 Trisog All Rights Reserved</span>
+            <span className="text-gray-400 text-sm">
+              © 2023 Trisog All Rights Reserved
+            </span>
           </div>
         </div>
       </footer>
-      <div className="w-full h-1 bg-rose-500"/>
+      <div className="w-full h-1 bg-rose-500" />
     </>
-  )
+  );
 }
--- src/pages/Home/FourthSection/athoms/OutlineButton.tsx
+++ src/pages/Home/FourthSection/athoms/OutlineButton.tsx
@@ -1,6 +1,9 @@
+import { useNavigate } from "react-router"
+
 export default function OutlineButton() {
+  const navigate = useNavigate();
   return (
-    <button className="px-8 w-fit py-4 rounded-lg border border-gray-300">
+    <button className="px-8 w-fit py-4 rounded-lg border border-gray-300" onClick={() => navigate('/error')}>
       <span className="font-display text-black font-bold text-base text-center flex-1">Contact Us</span>
     </button>
   )
