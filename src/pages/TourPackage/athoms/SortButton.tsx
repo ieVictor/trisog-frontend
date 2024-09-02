10ffa459ec4c83a266dd0ef3d1a2974b00b1efaa
Victor Gabriel <victor_tiss@outlook.com>
2024-09-02 11:25:57 -0300

    feat(tourPackage): add sortButton action

--- src/pages/TourPackage/athoms/SortButton.tsx
+++ src/pages/TourPackage/athoms/SortButton.tsx
@@ -1,15 +1,26 @@
 import { SortAscending, SortDescending } from "@phosphor-icons/react";
 import { useState } from "react";
 
-export default function SortButton() {
-  const [sort, setSort] = useState<boolean>(true);
+type SortButtonProps = {
+  onClick: (direction: string) => void;
+}
+
+export default function SortButton(props: SortButtonProps) {
+  const [direction, setDiretion] = useState<boolean>(true);
+  const [value, setValue] = useState<string>('asc');
+
+  const handleClick = () => {
+    setDiretion(!direction);
+    setValue(value === 'asc' ? 'desc' : 'asc');
+    props.onClick(value);
+  }
 
   return (
     <div 
       className="w-fit p-2 hover:bg-gray-200 rounded text-gray-500"
-      onClick={() => setSort(!sort)}
+      onClick={handleClick}
     >
-      { sort 
+      { direction 
         ? <SortAscending size={24}/> 
         : <SortDescending size={24}/>
       }
