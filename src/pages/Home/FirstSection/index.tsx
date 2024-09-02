8550c4d9e96cf846ba60d45347a6ebd6d9fa2428
Victor Gabriel <victor_tiss@outlook.com>
2024-09-02 11:20:44 -0300

    chore(home): format code

--- src/pages/Home/FirstSection/index.tsx
+++ src/pages/Home/FirstSection/index.tsx
@@ -1,14 +1,18 @@
-import DestinationForm from "../../../components/DestinationForm";
+import Search from '../../../components/Search';
 
 export default function FirstSection() {
   return (
     <section className="relative w-full h-[556px] flex flex-col text-center justify-center g-16 bg-home-first-section bg-cover">
-      <h2 className="font-script text-rose-500 text-4xl">Save 15% off in Worldwide</h2>
+      <h2 className="font-script text-rose-500 text-4xl">
+        Save 15% off in Worldwide
+      </h2>
       <div className="font-display text-white">
         <h1 className="text-[56px] font-bold">Travel & Adventures</h1>
-        <span className="text-md font-semibold">Find awesome hotel, tour, car and activities in London</span>
+        <span className="text-md font-semibold">
+          Find awesome hotel, tour, car and activities in London
+        </span>
       </div>
-      <DestinationForm/>
+      <Search />
     </section>
-  )
+  );
 }
