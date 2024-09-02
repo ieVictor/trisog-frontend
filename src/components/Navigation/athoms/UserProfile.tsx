b686275164a1854c4a86f8c2d83a4a6f4c318874
Victor Gabriel <victor_tiss@outlook.com>
2024-09-02 11:11:50 -0300

    feat(navigation): change src for avatar img & format code

--- src/components/Navigation/athoms/UserProfile.tsx
+++ src/components/Navigation/athoms/UserProfile.tsx
@@ -1,46 +1,50 @@
-import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@nextui-org/react"
-import { useUser } from "../../../hooks/userHooks"
-import { SignOut } from "@phosphor-icons/react"
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from '@nextui-org/react';
+import { useUser } from '../../../hooks/userHooks';
+import { SignOut } from '@phosphor-icons/react';
 
 type UserProfileProps = {
- imgURL?: string
- username?: string
-}
+  imgURL?: string;
+  username?: string;
+};
 
 export default function UserProfile(props: UserProfileProps) {
-  const DEFAULT_URL = 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png'
+  const DEFAULT_URL = 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png';
   const userHooks = useUser();
 
   const handleLogout = () => {
     userHooks.auth.signOut();
-  }
+  };
 
   return (
     <Dropdown>
       <DropdownTrigger>
-        <Avatar 
-          src={props.imgURL === DEFAULT_URL ? '' : props.imgURL} 
+        <Avatar
+          src={props.imgURL === DEFAULT_URL ? '' : props.imgURL}
           name={props.imgURL === DEFAULT_URL ? props.username : ''}
           color="danger"
-          className='rounded-full cursor-pointer'
+          className="rounded-full cursor-pointer"
           size="sm"
           isBordered
           radius="sm"
         />
       </DropdownTrigger>
       <DropdownMenu variant="bordered" color="primary">
-        <DropdownItem 
-          key="logout" 
-          className="text-gray-800" 
-          color="danger" 
-          onClick={handleLogout} 
-          endContent={(
-          <SignOut size={16}/>
-          )}
+        <DropdownItem
+          key="logout"
+          className="text-gray-800"
+          color="danger"
+          onClick={handleLogout}
+          endContent={<SignOut size={16} />}
         >
           Log out
         </DropdownItem>
       </DropdownMenu>
     </Dropdown>
-  )
-} 
+  );
+}
