e54b96734d9f3d5e7c662430a75b12aa17f003c4
Victor Gabriel <victor_tiss@outlook.com>
2024-09-02 11:22:08 -0300

    feat(home): add informations & format code

--- src/pages/Home/FourthSection/index.tsx
+++ src/pages/Home/FourthSection/index.tsx
@@ -1,37 +1,45 @@
-import Slider from 'react-slick'
-import PhotoDump from '../../../assets/WatchNow.png'
-import LeftSubtitle from '../../../components/LeftSubtitle'
-import CheckMark from './athoms/CheckMark'
-import OutlineButton from './athoms/OutlineButton'
-import TourTypeCard from './athoms/TourTypeCard'
-import CenteredTitle from '../../../components/CenteredTitle'
+import Slider from 'react-slick';
+import PhotoDump from '../../../assets/WatchNow.png';
+import LeftSubtitle from '../../../components/LeftSubtitle';
+import CheckMark from './athoms/CheckMark';
+import OutlineButton from './athoms/OutlineButton';
+import TourTypeCard from './athoms/TourTypeCard';
+import CenteredTitle from '../../../components/CenteredTitle';
+import { useHomeData } from '../../../hooks/useHomeData';
 export default function FourthSection() {
+  const { categories } = useHomeData();
+
   return (
     <section className="w-full bg-white gap-16 p-32 pt-8 flex flex-col">
-      <div className='flex flex-col gap-16 w-full text-start'>
-        <div className='flex flex-row justify-between items-center'>
-          <img src={PhotoDump} alt='Photographies'/>
-          <div className='flex flex-col max-w-[380px] gap-4'>
-            <hgroup className='w-full gap-5 flex flex-col items-start'>
-              <LeftSubtitle content='Why Choose Us'/>
-              <h1 className='text-blue-950 font-display font-bold text-4xl'>Our Experiences Meet High Quality Standards</h1>
+      <div className="flex flex-col gap-16 w-full text-start">
+        <div className="flex flex-row justify-between items-center">
+          <img src={PhotoDump} alt="Photographies" />
+          <div className="flex flex-col max-w-[380px] gap-4">
+            <hgroup className="w-full gap-5 flex flex-col items-start">
+              <LeftSubtitle content="Why Choose Us" />
+              <h1 className="text-blue-950 font-display font-bold text-4xl">
+                Our Experiences Meet High Quality Standards
+              </h1>
             </hgroup>
-            <data className='flex flex-col w-full gap-11'>
-              <span className='text-gray-600'>Holisticly optimize proactive strategic theme areas rather than effective manufactured products create.</span>
-              <div className='flex flex-wrap justify-between gap-4 pr-8'>
-                <CheckMark content='Cheap Rates'/>
-                <CheckMark content='Cheap Rates'/>
-                <CheckMark content='Hand-picked Tour'/>
-                <CheckMark content='Private Guide'/>
+            <data className="flex flex-col w-full gap-11">
+              <span className="text-gray-600">
+                Holisticly optimize proactive strategic theme areas rather than
+                effective manufactured products create.
+              </span>
+              <div className="flex flex-wrap justify-between gap-4 pr-8">
+                <CheckMark content="Cheap Rates" />
+                <CheckMark content="Cheap Rates" />
+                <CheckMark content="Hand-picked Tour" />
+                <CheckMark content="Private Guide" />
               </div>
-              <OutlineButton/>
+              <OutlineButton />
             </data>
           </div>
         </div>
-        <hr/>
+        <hr />
       </div>
-      <CenteredTitle title='Pick a Tour Type' subtitle='Browse By Category'/>
-      <div className='w-full'>
+      <CenteredTitle title="Pick a Tour Type" subtitle="Browse By Category" />
+      <div className="w-full">
         <Slider
           infinite={false}
           dotsClass="slick-dots -bottom-16 text-rose-500"
@@ -39,14 +47,18 @@ export default function FourthSection() {
           speed={500}
           slidesToShow={6}
         >
-          <TourTypeCard/>
-          <TourTypeCard/>
-          <TourTypeCard/>
-          <TourTypeCard/>
-          <TourTypeCard/>
-          <TourTypeCard/>
+          {categories &&
+            categories.map((category) => (
+              <TourTypeCard
+                key={category.id}
+                categoryId={category.id}
+                categoryName={category.name}
+                lowestPrice={category.lowestPrice ? category.lowestPrice : 0}
+                tourCounter={category.tourCount ? category.tourCount : 0}
+              />
+            ))}
         </Slider>
       </div>
     </section>
-  )
+  );
 }
