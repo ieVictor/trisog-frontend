d5e164f49bc4ad2792035562ca79eae8fa8f5484
Victor Gabriel <victor_tiss@outlook.com>
2024-09-02 11:16:46 -0300

    feat(types): add categories & tours types

--- src/types/TourType.ts
+++ src/types/TourType.ts
@@ -1,32 +1,3 @@
-export type TourInformations = {
-  duration: number;
-  minAge: number;
-  from: number;
-  maxPeople: number;
-  tourType: string;
-};
-
-export type TourReviews = {
-  counter: number;
-  average: number;
-  services: number;
-  locations: number;
-  amenities: number;
-  prices: number;
-  food: number;
-  room: number;
-};
-
-export type TourProps = {
-  data: {
-    title: string;
-    overview: string;
-    location: string;
-    informations: TourInformations;
-    reviews: TourReviews;
-  };
-};
-
 export type Tour = {
   id: string;
   title: string;
@@ -94,4 +65,12 @@ export type Ratings = {
   prices: number;
   food: number;
   room: number;
+}
+
+export type PaginationTourResponse = {
+  page: number;
+  limit: number;
+  total_pages: number;
+  total_tours: number;
+  tours: Tour[];
 }
